ffe262471d92fd68818512404619903027ccdf92
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-03-23 23:15:31 -0400

    Simplify main collection function

--- bci-desktop-file-utils.el
+++ bci-desktop-file-utils.el
@@ -21,25 +21,15 @@
             (nconc key-value-pairs (list key (match-string 2)))))))))
 
 ;;;###autoload
-(defun bci-parse-all-desktop-files ()
+(defun bci-collect-all-desktop-objects ()
   "Proof of concept function that reads the filesystem for
 desktop files, analyzing each corresponding program's set of categories.
 
 The idea is to eventually make a tool that generates a Fluxbox menu."
   (let (main-list)
     (dolist (desktop-file (directory-files "/usr/share/applications" t "^.+\.desktop$") main-list)
-      (with-temp-buffer
-        (insert-file-contents desktop-file)
-        (let* ((lines (string-lines (string-chop-newline (buffer-string))))
-               (categories-line
-                (seq-find
-                 (lambda (line) (string-match "^Categories" line))
-                 lines)))
-          (when categories-line
-            (let* ((raw-categories (progn (string-match "^Categories=\\(.+\\)$" categories-line) (match-string 1 categories-line)))
-                   (categories-tokens (butlast (split-string raw-categories ";"))))
-              (push (cons desktop-file (list categories-tokens)) main-list))))
-        ))))
+      (push (--bci-create-desktop-object desktop-file "Exec" "Categories") main-list))))
+
 
 ;; https://specifications.freedesktop.org/menu-spec/menu-spec-1.0.html
 ;; https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
